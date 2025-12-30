c20c9c9df810b639cfa7983bc5b7b985665b7ea5
benoit harel <bharel@etik.com>
2025-12-30 15:39:26 +0100

    [ADD] Move skills data to a separate file for better organization and maintainability

---
+++ data/skills.js
@@ -0,0 +1,32 @@
+export const skills = [
+      {
+        id: 1,
+        name: "HTML5",
+        percentage: 90,
+        color: "danger",
+      },
+      {
+        id: 2,
+        name: "CSS3",
+        percentage: 80,
+        color: "info",
+      },
+      {
+        id: 3,
+        name: "JAVASCRIPT",
+        percentage: 70,
+        color: "warning",
+      },
+      {
+        id: 4,
+        name: "PHP",
+        percentage: 60,
+        color: "success",
+      },
+      {
+        id: 5,
+        name: "REACT",
+        percentage: 50,
+        color: "primary",
+      },
+    ];
