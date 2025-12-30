59b31e24a2eeadae60d1751830fca9570e6efe52
benoit harel <bharel@etik.com>
2025-12-30 16:05:33 +0100

    [ADD] Create services data file to organize service offerings

---
+++ data/services.js
@@ -0,0 +1,23 @@
+export const services = [
+  {
+    id: 1,
+    icon: "pencil-square",
+    title: "UX Design",
+    description:
+      "L'UX Design est une discipline qui consiste à concevoir des produits (sites web, applications mobiles, logiciels, objets connectés, etc.) en plaçant l'utilisateur au cœur de la démarche pour offrir l'expérience utilisateur la plus fluide et agréable possible.",
+  },
+  {
+    id: 2,
+    icon: "code-slash",
+    title: "Développement web",
+    description:
+      "Le développement de sites web consiste à créer des websites en utilisant des langages de programmation (HTML, CSS, JavaScript, PHP, etc.) et des frameworks (Bootstrap, React, Angular, etc.) pour mettre en œuvre des interfaces web fonctionnelles et performantes.",
+  },
+  {
+    id: 3,
+    icon: "search",
+    title: "Référencement",
+    description:
+      "Le référencement naturel (SEO) est une technique qui consiste à optimiser un site web pour faire remonter dans les résultats des moteurs de recherche (Google, Bing, etc.). L'objectif est d'attirer un maximum de visiteurs qualifiés sur le site.",
+  },
+];
